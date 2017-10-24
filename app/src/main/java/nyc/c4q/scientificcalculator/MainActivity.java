5619327bf5222a97e80655df5c72e620da4906ff
Oleg Siboglov <olegsiboglov@ac.c4q.nyc>
2017-10-23 20:14:47 -0400

    first commit

---
+++ app/src/main/java/nyc/c4q/scientificcalculator/MainActivity.java
@@ -0,0 +1,221 @@
+package nyc.c4q.scientificcalculator;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
+
+import nyc.c4q.scientificcalculator.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    private static final char addition = '+';
+    private static final char subtraction = '-';
+    private static final char multiplication = '*';
+    private static final char division = '/';
+    private char calculations;
+    private double valueFirst = Double.NaN;
+    private double valueSecond = Double.NaN;
+
+    private DecimalFormat decimalFormat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        decimalFormat = new DecimalFormat("#.##########");
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        binding.buttonDot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText(binding.editText.getText() + ".");
+            }
+        });
+
+        binding.buttonZero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText((binding.editText.getText() + "0"));
+            }
+        });
+
+        binding.buttonOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText((binding.editText.getText() + "1"));
+            }
+        });
+
+        binding.buttonTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText((binding.editText.getText() + "2"));
+            }
+        });
+
+        binding.buttonThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText((binding.editText.getText() + "3"));
+            }
+        });
+
+        binding.buttonFour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText((binding.editText.getText() + "4"));
+            }
+        });
+
+        binding.buttonFive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText((binding.editText.getText() + "5"));
+            }
+        });
+
+        binding.buttonSix.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText((binding.editText.getText() + "6"));
+            }
+        });
+
+        binding.buttonSeven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText((binding.editText.getText() + "7"));
+            }
+        });
+
+        binding.buttonEight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText((binding.editText.getText() + "8"));
+            }
+        });
+
+        binding.buttonNine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText((binding.editText.getText() + "9"));
+            }
+        });
+
+        binding.buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                unlimtedPower();
+                calculations = addition;
+                if (!Double.isNaN(valueFirst)) {
+                    binding.textView.setText(decimalFormat.format(valueFirst) + "+");
+                    binding.editText.setText(null);
+                }
+            }
+        });
+
+        binding.buttonSubtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                unlimtedPower();
+                calculations = subtraction;
+                if (!Double.isNaN(valueFirst)) {
+                    binding.textView.setText(decimalFormat.format(valueFirst) + "-");
+                    binding.editText.setText(null);
+                }
+            }
+        });
+
+        binding.buttonDivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                unlimtedPower();
+                calculations = division;
+                if (!Double.isNaN(valueFirst)) {
+                    binding.textView.setText(decimalFormat.format(valueFirst) + "/");
+                    binding.editText.setText(null);
+                }
+            }
+        });
+
+        binding.buttonMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                unlimtedPower();
+                calculations = multiplication;
+                if (!Double.isNaN(valueFirst)) {
+                    binding.textView.setText(decimalFormat.format(valueFirst) + "*");
+                    binding.editText.setText(null);
+                }
+            }
+        });
+
+        binding.buttonEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                unlimtedPower();
+                if (!Double.isNaN(valueSecond)) {
+                    try {
+                        binding.textView.setText(binding.textView.getText().toString() + decimalFormat.format(valueSecond) + " = " + decimalFormat.format(valueFirst));
+                        valueFirst = Double.NaN;
+                        calculations = '0';
+                    } catch (Exception e) {
+                        Toast.makeText(getApplicationContext(), "The number you have entered is not a number.", Toast.LENGTH_LONG).show();
+                        binding.editText.setText(null);
+                        binding.textView.setText(null);
+                    }
+                }
+            }
+        });
+
+        binding.buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (binding.editText.getText().length() > 0) {
+                    CharSequence currentText = binding.editText.getText();
+                    binding.editText.setText(currentText.subSequence(0, currentText.length()-1));
+                } else {
+                    valueFirst = Double.NaN;
+                    valueSecond = Double.NaN;
+                    binding.editText.setText("");
+                    binding.textView.setText("");
+                }
+            }
+        });
+    }
+
+    private void unlimtedPower() {
+        if (!Double.isNaN(valueFirst)) {
+            valueSecond = Double.parseDouble(binding.editText.getText().toString());
+            binding.editText.setText(null);
+
+            if (calculations == addition) {
+                valueFirst = this.valueFirst + valueSecond;
+            } else if (calculations == subtraction) {
+                valueFirst = this.valueFirst - valueSecond;
+            } else if (calculations == division) {
+                valueFirst = this.valueFirst / valueSecond;
+            } else if (calculations == multiplication) {
+                valueFirst = this.valueFirst * valueSecond;
+            }
+        }
+        else {
+            try {
+                valueFirst = Double.parseDouble(binding.editText.getText().toString());
+            }
+            catch (Exception e) {
+                Toast.makeText(getApplicationContext(), "The number you have entered is not a number.", Toast.LENGTH_LONG).show();
+                binding.editText.setText(null);
+            }
+        }
+    }
+}
+
+
